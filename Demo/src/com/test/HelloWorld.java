4f0e6ecf5859db1bb1f12a67d56b502cee3107e1
Raining <trowe.wny@gmail.com>
2019-10-29 19:48:39 +0800

    二次提交

--- Demo/src/com/test/HelloWorld.java
+++ Demo/src/com/test/HelloWorld.java
@@ -3,8 +3,8 @@ package com.test;
 public class HelloWorld {
 public static void main(String[] args) {
 
-	System.out.println("one commit ");
-	System.out.println("write");
-	System.out.println("season");
+	System.out.println("two commit ");
+	System.out.println("blick");
+	System.out.println("spring");
 }
 }
